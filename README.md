fbe19d7f80b5ed51a4ca66d61fcfac6e239516dd
emiliano <thayalinux@gmail.com>
2013-08-21 20:59:41 -0300

    First commit

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-thaya-perfiles-
-===============
+thaya-perfiles
+==============
 
 Perfiles para la construccción de distribuciones Thaya Linux
+
+--------------
+Versiones
+
+* thaya_1.0a-lb_3.x Inicio rama de desarrollo con live-build 3.x
+* 0.0   Prueba de concepto
+
---
+++ thaya/chroot-local-hook.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+apt-get clean
+apt-get autoclean
+
+[ -e /var/cache/apt/pkgcache.bin ] && rm -rf /var/cache/apt/pkgcache.bin
+[ -e /var/cache/apt/srcpkgcache.bin ] && rm -rf /var/cache/apt/srcpkgcache.bin
+
+rm -rf /var/lib/apt/lists/repositorio*
+rm -rf /var/lib/apt/lists/seguridad*
+rm -rf /var/lib/apt/lists/universo*
+
+#[ -e /usr/share/icons/gnome/icon-theme.cache ] && rm -rf /usr/share/icons/gnome/icon-theme.cache
+#[ -e /usr/share/icons/canaima-iconos/icon-theme.cache ] && rm -rf /usr/share/icons/canaima-iconos/icon-theme.cache
+
+
